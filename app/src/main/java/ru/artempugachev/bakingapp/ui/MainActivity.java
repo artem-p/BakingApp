348d27ca27a903bb7c8e7c033e6da90e3cae378e
Artem Pugachev <artempugachev1@gmail.com>
2017-09-12 22:14:33 +0300

    okhttp dependency

--- app/src/main/java/ru/artempugachev/bakingapp/ui/MainActivity.java
+++ app/src/main/java/ru/artempugachev/bakingapp/ui/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import ru.artempugachev.bakingapp.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String RECIPE_JSON_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
