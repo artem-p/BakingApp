6e073cf1100917451e22a6b802337938f430e007
artem-p <artempugachev1@gmail.com>
2017-09-15 17:56:52 +0300

    adapter for steps recycler

--- app/src/main/java/ru/artempugachev/bakingapp/ui/recipedetails/StepsAdapter.java
+++ app/src/main/java/ru/artempugachev/bakingapp/ui/recipedetails/StepsAdapter.java
@@ -1,9 +1,62 @@
 package ru.artempugachev.bakingapp.ui.recipedetails;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ru.artempugachev.bakingapp.R;
+import ru.artempugachev.bakingapp.model.Step;
+import ru.artempugachev.bakingapp.ui.RecipeAdapter;
+
 /**
  * Adapter for steps recycler view
  */
 
-public class StepsAdapter {
+public final class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepsViewHolder> {
+    private List<Step> steps;
 
+    public StepsAdapter(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    @Override
+    public StepsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int recipeCardLayoutId = R.layout.step_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(recipeCardLayoutId, parent, shouldAttachToParentImmediately);
+        return new StepsAdapter.StepsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(StepsViewHolder holder, int position) {
+        if (steps != null && !steps.isEmpty()) {
+            Step step = steps.get(position);
+            holder.stepTitle.setText(step.asText());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return steps != null ? steps.size() : 0;
+    }
+
+    public final class StepsViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.stepTitle)
+        TextView stepTitle;
+
+        public StepsViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
 }
