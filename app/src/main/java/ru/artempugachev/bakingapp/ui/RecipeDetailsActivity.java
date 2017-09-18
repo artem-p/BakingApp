23cd4dc6eb0274b8d83779d18130a8316719cbb8
Artem Pugachev <artempugachev1@gmail.com>
2017-09-18 20:57:50 +0300

    recipe details fragment

--- app/src/main/java/ru/artempugachev/bakingapp/ui/RecipeDetailsActivity.java
+++ app/src/main/java/ru/artempugachev/bakingapp/ui/RecipeDetailsActivity.java
@@ -1,27 +1,17 @@
 package ru.artempugachev.bakingapp.ui;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import ru.artempugachev.bakingapp.R;
 import ru.artempugachev.bakingapp.model.Recipe;
 
-public class RecipeDetailsActivity extends AppCompatActivity implements StepsAdapter.StepClickListener {
-    @BindView(R.id.ingredientsTextView)
-    TextView ingredientsTextView;
-
-    @BindView(R.id.stepsRecycler)
-    RecyclerView stepsRecycler;
-
-    private StepsAdapter stepsAdapter;
-
+public class RecipeDetailsActivity extends AppCompatActivity  {
     private Recipe recipe = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,44 +25,19 @@ public class RecipeDetailsActivity extends AppCompatActivity implements StepsAda
             recipe = intent.getParcelableExtra(MainActivity.RECIPE_EXTRA);
         }
 
+        RecipeDetailsFragment recipeDetailsFragment = new RecipeDetailsFragment();
+
+        Bundle arguments = new Bundle();
+        arguments.putParcelable(MainActivity.RECIPE_EXTRA, recipe);
+        recipeDetailsFragment.setArguments(arguments);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .add(R.id.recipe_details_fragment_container, recipeDetailsFragment)
+                .commit();
+
         if (recipe != null) {
-            fillViews(recipe);
-        } else {
-            showNoRecipe();
+            setTitle(recipe.getName());
         }
-    }
-
-    /**
-     * Fill recipe views with data
-     * */
-    private void fillViews(Recipe recipe) {
-        setTitle(recipe.getName());
-
-        String ingredientsText = recipe.toIngredientsText();
-        ingredientsTextView.setText(ingredientsText);
-
-        LinearLayoutManager stepsLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        stepsRecycler.setLayoutManager(stepsLayoutManager);
-
-        stepsAdapter = new StepsAdapter(recipe.getSteps(), this);
-        stepsRecycler.setAdapter(stepsAdapter);
-    }
-
-    /**
-     * We have no recipe, show error text
-     * */
-    private void showNoRecipe() {
-
-    }
-
-
-    /**
-     * Handle click on step. Show step details.
-     * */
-    @Override
-    public void onStepClick(int stepPosition) {
-        Intent intent = new Intent(RecipeDetailsActivity.this, StepActivity.class);
-        intent.putExtra(MainActivity.STEP_EXTRA, recipe.getStep(stepPosition));
-        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ru/artempugachev/bakingapp/ui/RecipeDetailsFragment.java
@@ -0,0 +1,92 @@
+package ru.artempugachev.bakingapp.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ru.artempugachev.bakingapp.R;
+import ru.artempugachev.bakingapp.model.Recipe;
+
+/**
+ * List of ingredients and steps to prepare
+ */
+
+public class RecipeDetailsFragment extends Fragment implements StepsAdapter.StepClickListener {
+    @BindView(R.id.ingredientsTextView)
+    TextView ingredientsTextView;
+
+    @BindView(R.id.stepsRecycler)
+    RecyclerView stepsRecycler;
+
+    private StepsAdapter stepsAdapter;
+
+    private Recipe recipe;
+
+    public RecipeDetailsFragment() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.recipe_details_fragment, container, false);
+        ButterKnife.bind(this, rootView);
+
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            recipe = arguments.getParcelable(MainActivity.RECIPE_EXTRA);
+
+            if (recipe != null) {
+                fillViews(recipe);
+            } else {
+                showNoRecipe();
+            }
+        }
+
+        return rootView;
+    }
+
+
+    /**
+     * Fill recipe views with data
+     * */
+    public void fillViews(Recipe recipe) {
+        String ingredientsText = recipe.toIngredientsText();
+        ingredientsTextView.setText(ingredientsText);
+
+        LinearLayoutManager stepsLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        stepsRecycler.setLayoutManager(stepsLayoutManager);
+
+        stepsAdapter = new StepsAdapter(recipe.getSteps(), this);
+        stepsRecycler.setAdapter(stepsAdapter);
+    }
+
+    /**
+     * We have no recipe, show error text
+     * */
+    public void showNoRecipe() {
+
+    }
+
+
+    /**
+     * Handle click on step. Show step details.
+     * */
+    @Override
+    public void onStepClick(int stepPosition) {
+        if (recipe != null) {
+            Intent intent = new Intent(getActivity(), StepActivity.class);
+            intent.putExtra(MainActivity.STEP_EXTRA, recipe.getStep(stepPosition));
+            startActivity(intent);
+        }
+    }
+}
