79888ed031046f556e185af19d9b2896ff176484
artem-p <artempugachev1@gmail.com>
2017-09-26 18:05:27 +0300

    ingredients list service stub

--- app/src/main/java/ru/artempugachev/bakingapp/ui/widget/IngredientsListService.java
+++ app/src/main/java/ru/artempugachev/bakingapp/ui/widget/IngredientsListService.java
@@ -1,8 +1,11 @@
 package ru.artempugachev.bakingapp.ui.widget;
 
+import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import ru.artempugachev.bakingapp.R;
 
 /**
  * Remote views service to display ingredients list.
@@ -11,13 +14,17 @@ import android.widget.RemoteViewsService;
 public class IngredientsListService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return new IngredientsListViewFactory();
+        return new IngredientsListViewFactory(this.getApplicationContext(), intent);
     }
 
 }
 
 class IngredientsListViewFactory implements RemoteViewsService.RemoteViewsFactory {
+    private Context context;
 
+    public IngredientsListViewFactory(Context applicationContext, Intent intent) {
+        this.context = context;
+    }
 
     @Override
     public void onCreate() {
@@ -36,12 +43,15 @@ class IngredientsListViewFactory implements RemoteViewsService.RemoteViewsFactor
 
     @Override
     public int getCount() {
-        return 0;
+        return 3; // todo stub
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
-        return null;
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget_list_item);
+        remoteViews.setTextViewText(R.id.ingredients_widget_list_item_text, String.valueOf(position));
+
+        return remoteViews;
     }
 
     @Override
@@ -51,12 +61,12 @@ class IngredientsListViewFactory implements RemoteViewsService.RemoteViewsFactor
 
     @Override
     public int getViewTypeCount() {
-        return 0;
+        return 1;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
