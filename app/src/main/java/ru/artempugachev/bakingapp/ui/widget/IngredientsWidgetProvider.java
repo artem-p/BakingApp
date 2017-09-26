fef71624ff5ded426cb072d77bd8f3890e2f3a78
artem-p <artempugachev1@gmail.com>
2017-09-26 18:29:37 +0300

    widget provider

--- app/src/main/java/ru/artempugachev/bakingapp/ui/widget/IngredientsWidgetProvider.java
+++ app/src/main/java/ru/artempugachev/bakingapp/ui/widget/IngredientsWidgetProvider.java
@@ -13,35 +13,44 @@ import ru.artempugachev.bakingapp.R;
  */
 
 public class IngredientsWidgetProvider extends AppWidgetProvider {
-    public static final String WIDGET_IDS_EXTRA = "widget_ids";
-    public static final String INGREDIENTS_TEXT_EXTRA = "ingredients_text";
-
-
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] widgetIds) {
-        update(context, appWidgetManager, widgetIds, context.getResources().getString(R.string.ingredients_widget_default_text));
-    }
-
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (intent.hasExtra(WIDGET_IDS_EXTRA)) {
-            int[] widgetIds = intent.getExtras().getIntArray(WIDGET_IDS_EXTRA);
-            if (intent.hasExtra(INGREDIENTS_TEXT_EXTRA)) {
-                String ingredientsList = intent.getStringExtra(INGREDIENTS_TEXT_EXTRA);
-                update(context, AppWidgetManager.getInstance(context), widgetIds, ingredientsList);
-            } else {
-                onUpdate(context, AppWidgetManager.getInstance(context), widgetIds);
-            }
-        } else super.onReceive(context, intent);
-    }
-
-
-    private void update(Context context, AppWidgetManager appWidgetManager, int[] widgetIds, String ingredients) {
-        for (int widgetId : widgetIds) {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
-            views.setTextViewText(R.id.ingredients_widget_list, ingredients);
+            Intent widgetServiceIntent = new Intent(context, IngredientsListService.class);
+            views.setRemoteAdapter(R.id.ingredients_widget_ingredients_list, widgetServiceIntent);
             appWidgetManager.updateAppWidget(widgetId, views);
         }
     }
+    //    public static final String WIDGET_IDS_EXTRA = "widget_ids";
+//    public static final String INGREDIENTS_TEXT_EXTRA = "ingredients_text";
+//
+//
+//    @Override
+//    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] widgetIds) {
+//        update(context, appWidgetManager, widgetIds, context.getResources().getString(R.string.ingredients_widget_default_text));
+//    }
+//
+//
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        if (intent.hasExtra(WIDGET_IDS_EXTRA)) {
+//            int[] widgetIds = intent.getExtras().getIntArray(WIDGET_IDS_EXTRA);
+//            if (intent.hasExtra(INGREDIENTS_TEXT_EXTRA)) {
+//                String ingredientsList = intent.getStringExtra(INGREDIENTS_TEXT_EXTRA);
+//                update(context, AppWidgetManager.getInstance(context), widgetIds, ingredientsList);
+//            } else {
+//                onUpdate(context, AppWidgetManager.getInstance(context), widgetIds);
+//            }
+//        } else super.onReceive(context, intent);
+//    }
+//
+//
+//    private void update(Context context, AppWidgetManager appWidgetManager, int[] widgetIds, String ingredients) {
+//        for (int widgetId : widgetIds) {
+//            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
+//            views.setTextViewText(R.id.ingredients_widget_list, ingredients);
+//            appWidgetManager.updateAppWidget(widgetId, views);
+//        }
+//    }
 }
