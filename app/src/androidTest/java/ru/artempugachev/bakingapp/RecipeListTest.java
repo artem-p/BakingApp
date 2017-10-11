82db9b9e8c5dc225378570869808141cdc4f111e
artem-p <artempugachev1@gmail.com>
2017-10-11 15:34:05 +0300

    test correct recipe details activity header

--- app/src/androidTest/java/ru/artempugachev/bakingapp/RecipeListTest.java
+++ app/src/androidTest/java/ru/artempugachev/bakingapp/RecipeListTest.java
@@ -4,9 +4,12 @@ package ru.artempugachev.bakingapp;
  * Testing recipe list
  */
 
+import android.content.Intent;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,8 +20,12 @@ import ru.artempugachev.bakingapp.ui.activity.MainActivity;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 
 /**
  * Test recipe list
@@ -29,12 +36,32 @@ public class RecipeListTest {
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     /**
-     * Clicks on recipe in list
-     * Checks it opens up the RecipeDetails
+     * Click on recipe in list
+     * Check it opens up the RecipeDetails
      * */
     @Test
     public void clickRecipeItem_opensDetailsActivity() {
         onView(withId(R.id.recipe_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
         onView(withId(R.id.ingredientsTextView)).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Click on recipe in list
+     * Check correct recipe name in details activity header
+     * */
+    @Test
+    public void clickRecipeItem_detailsWithCorrectHeader() {
+        onView(withId(R.id.recipe_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(allOf(
+                isAssignableFrom(TextView.class),
+                withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Nutella Pie")));
+
+        activityTestRule.launchActivity(new Intent());
+        onView(withId(R.id.recipe_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(allOf(
+                isAssignableFrom(TextView.class),
+                withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText("Brownies")));
     }
 }
---
+++ app/src/androidTest/java/ru/artempugachev/bakingapp/StepDetailsTest.java
@@ -0,0 +1,8 @@
+package ru.artempugachev.bakingapp;
+
+/**
+ * Created by artem on 11.10.17.
+ */
+
+public class StepDetailsTest {
+}
